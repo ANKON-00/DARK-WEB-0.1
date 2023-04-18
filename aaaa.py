c5c31711d2fb68e3379849f4968c96d154704a6e
ANKON-00 <128901218+ANKON-00@users.noreply.github.com>
2023-04-18 20:27:28 +0600

    Add files via upload

---
+++ aaaa.py
@@ -0,0 +1,18 @@
+#(:-------------INFU-----------:)
+#(:-------DECOMPILE BY ANKON CYBER-------:)
+#(:------FACEBOOK->X'ten Ankon--------:)
+# (:-------WHATS APP ->01855598096------:)
+#(:--------GITHUP-> ANKON-00-------:)
+
+
+
+
+
+import os
+print("\033[1;92m [•] ANKON CYBER (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [•] ROBOT INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [•] UPDATE CHECKING \033[1;30m")
+os.system('espeak -a 300 "WELLCOME  TO BANGLADESH CYBER KING OF ANKON  MOOD. WELLCOME TO DIP MOMDAL.     HOW CAN I HELP YOU.  A TO Z "')
+os.system("git pull")
